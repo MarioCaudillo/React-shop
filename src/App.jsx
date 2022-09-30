0bbcb21e01a0adc929f3feb68ab7e89beebca822
MarioCaudillo <caudilloluis116@gmail.com>
2022-09-29 20:17:49 -0500

    Configuracion inicial de proyecto en React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import  React from 'react';
+import  '../styles/global.scss';
+
+function    App(){
+    return(
+        <div className=''>
+            <h1>Good Morning!</h1>
+        </div>
+    );
+}
+
+export default  App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path=require('path');
+const MiniCssExtractPlugin  =   require('mini-css-extract-plugin');
+
+module.exports  =   {
+    entry:  './src/index.js',
+    output: {
+        filename:   'bundle.[hash].js',
+        path:   path.resolve(__dirname, 'dist')
+    },
+    mode:   'production',
+    module: {
+        rules:  [
+            {
+                test: /\.(js|jsx)$/,
+                use:    'babel-loader',
+                exclude:    /node_modules/,
+                resolve:   {
+                    extensions: ['.js', '.jsx'],
+                },
+            },
+            {
+                test:   /\.s[ac]ss$/i,
+                use:    ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    plugins:    [new CleanWebpackPlugin(), new HtmlWebpackPlugin({
+        template:   './public/index.html',
+        filename:   './index.html'
+    }),
+     new MiniCssExtractPlugin(
+        {   filename: '[name].css'
+        
+        }),
+    ],
+    
+};
